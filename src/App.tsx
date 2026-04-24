80486725230c77fd283f010232ebc2f64bbe4382
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-04-24 17:50:06 +0800

    shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import {Button} from "@/components/ui/button"
+
 const App = () => {
     return (
         <div>
             this is app
+            <Button variant="outline">Button</Button>
         </div>
     );
 };
